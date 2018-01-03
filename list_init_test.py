404d448eef063d0e421a82ad4d00ae8abbccff4e
linhuaiyi <429062304@qq.com>
2018-01-03 23:03:19 +0800

    2018/1/3 23:02
    Simlarity score calculate between query and response

---
+++ list_init_test.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+
+new_file = ['祝', '汤', '教授', '新年', '快乐']
+new_vecotr = [0 for _ in range(233120)]
+print new_vecotr[-1]
---
+++ ltp_seg.py
@@ -0,0 +1,35 @@
+# encoding: utf-8
+
+import urllib2
+import urllib
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+def ltp_stc_seg(text, pattern):
+    uri_base = "http://api.ltp-cloud.com/analysis/?"
+    api_key = "Q1m0d4M2x1sZNRdm0g0M7qcBtrUrbmaIkMjFKsvI"
+    # text = "祝汤教授新年快乐"
+    # Note that if your text contain special characters such as linefeed or '&',
+    # you need to use urlencode to encode your data
+    text = urllib.quote(text)
+    format = "plain"
+    # pattern = "ws"
+
+    url = (uri_base + "api_key=" + api_key + "&" + "text=" + text + "&" + "pattern=" + pattern + "&"
+           + "format=" + format)
+
+    try:
+        response = urllib2.urlopen(url)
+        content = response.read().strip()
+        return content
+        print content
+    except urllib2.HTTPError, e:
+        print >> sys.stderr, e.reason
+
+if __name__ == '__main__':
+    test_text = "祝汤教授新年快乐"
+    test_pattern = "ws"
+    ltp_stc_seg(test_text, test_pattern)
