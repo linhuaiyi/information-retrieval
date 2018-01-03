1af8de3d252cfbb0e7638ab5477c1b640ae65cb9
linhuaiyi <429062304@qq.com>
2018-01-03 23:41:43 +0800

    Revert "2018/1/3 23:02"
    This reverts commit 404d448eef063d0e421a82ad4d00ae8abbccff4e.

--- jieba_seg.py
+++
@@ -1,7 +0,0 @@
-# -*- coding:utf-8 -*-
-
-import jieba
-
-
-def jieba_stc_seg(text):
-    return " ".join(jieba.cut(text))
--- list_init_test.py
+++
@@ -1,5 +0,0 @@
-# -*- coding:utf-8 -*-
-
-new_file = ['祝', '汤', '教授', '新年', '快乐']
-new_vecotr = [0 for _ in range(233120)]
-print new_vecotr[-1]
